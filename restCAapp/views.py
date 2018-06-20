7f75bc65c3e666065b8c38f2af5ab0da7a426d06
Joaoie <joaoiep@gmail.com>
2018-06-21 01:09:50 +0800

    Added initial UserViewSet
    Added initial UserViewSet to views

--- restCAapp/views.py
+++ restCAapp/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(ModelViewSet):
+    queryset = get_user_model().objects.all()
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+
+    def get_serializer_class(self):
+        return UserSerializer

bb656fd3a1f183d99cb710f11677ffd4596be19d
Joaoie <joaoiep@gmail.com>
2018-06-21 00:39:55 +0800

    Create user model
    creared user model

--- restCAapp/models.py
+++ restCAapp/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import UserManager
 from django.db import models
 
-# Create your models here.
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    is_active = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'

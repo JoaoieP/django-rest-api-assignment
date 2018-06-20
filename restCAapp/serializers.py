af20f73b3dbb419bf9c5e9c0fddcc316af972351
Joaoie <joaoiep@gmail.com>
2018-06-21 00:56:47 +0800

    Added REST framework and serializer
    Added rest framweork and serializer for user

---
+++ restCAapp/serializers.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from rest_framework.serializers import HyperlinkedModelSerializer
+
+
+class UserSerializer(HyperlinkedModelSerializer):
+    
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'password', 'first_name', 'last_name', 'is_active')
+        read_only_fields = ('is_active')
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create(**validated_data)
+        return user
